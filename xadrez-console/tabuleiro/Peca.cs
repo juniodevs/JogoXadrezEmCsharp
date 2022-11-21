e90ada7ee745fc4cf518687f27a83a26d053af67
Júnior Veras <iramarjunhorveras@gmail.com>
2022-11-21 16:03:16 -0300

    Implementando os movimentos possíveis do rei

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,13 +1,12 @@
 ﻿namespace tabuleiro
 {
-    class Peca
+    abstract class Peca 
     {
-
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
-        public Tabuleiro tab { get;  protected set; }
-        public Peca (Tabuleiro tab, Cor cor)
+        public Tabuleiro tab { get; protected set; }
+        public Peca(Tabuleiro tab, Cor cor)
         {
             this.posicao = null;
             this.tab = tab;
@@ -19,5 +18,7 @@
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
